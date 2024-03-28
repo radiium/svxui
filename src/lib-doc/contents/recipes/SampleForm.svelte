20bc8835417c9631abed5ce94ee6ad9723b28997
radiium <remip.dev@gmail.com>
2024-03-28 11:58:28 +0100

    fix: sample form config size

--- src/lib-doc/contents/recipes/SampleForm.svelte
+++ src/lib-doc/contents/recipes/SampleForm.svelte
@@ -36,7 +36,7 @@
                 name: 'size',
                 type: 'enum',
                 values: ['small', 'medium', 'large'],
-                default: 'm'
+                default: 'medium'
             },
             {
                 name: 'color',
