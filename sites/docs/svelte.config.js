34d2500617359a4ba5b46837d6a19626581aa582
radiium <remip.dev@gmail.com>
2024-10-16 20:22:35 +0200

    feat(sites/docs): add adapter-vercel

--- sites/docs/svelte.config.js
+++ sites/docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsx } from 'mdsx';
 import { readFileSync } from 'node:fs';
