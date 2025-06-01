2cf191681d74356918e48a5120140b2810960e19
radiium <remip.dev@gmail.com>
2025-06-01 08:07:54 +0200

    refactor(lib): remove useless plugin

--- packages/svxui/plugins/preprocessors/utils/type.d.ts
+++
@@ -1,5 +0,0 @@
-export type Alias = (typeof aliasNames)[number];
-export type AccentColor = (typeof accentColors)[number];
-export type GrayColor = (typeof grayColors)[number];
-export type Color = AccentColor | GrayColor;
-export type ThemeConfig = Record<Alias, Color>;
