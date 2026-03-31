7f0f04776773560b6779642b77eca69a629b136b
radiium <remip.dev@gmail.com>
2026-04-01 00:35:23 +0200

    feat(docs): add new svg icons

---
+++ apps/docs/src/lib/components/icons/EyeIcon.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import type { IconProps } from './types.js';
+    let { size = '20px', color = 'currentColor', mirrored = false, ...rest }: IconProps = $props();
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    fill={color}
+    transform={mirrored ? 'scale(-1, 1)' : undefined}
+    viewBox="0 0 256 256"
+    {...rest}
+>
+    <rect width="256" height="256" fill="none" /><path
+        d="M128,56C48,56,16,128,16,128s32,72,112,72,112-72,112-72S208,56,128,56Z"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+    />
+    <circle
+        cx="128"
+        cy="128"
+        r="40"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+    />
+</svg>
