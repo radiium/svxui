c29601745189cba2e7ae57cc6262e442351eb341
radiium <remip.dev@gmail.com>
2026-04-30 23:47:36 +0200

    feat(docs): disable recipes nav

--- apps/docs/src/routes/docs/recipes/[...slug]/+page.ts
+++ apps/docs/src/routes/docs/recipes/[...slug]/+page.ts
@@ -2,9 +2,9 @@ import { loadRecipeEntries } from '$lib/content-utils/load-entries.js';
 import { loadRecipePage } from '$lib/content-utils/load-page.js';
 import type { EntryGenerator, PageLoad } from './$types.js';
 
-export const entries: EntryGenerator = () => {
-    return loadRecipeEntries();
-};
+// export const entries: EntryGenerator = () => {
+//     return loadRecipeEntries();
+// };
 
 export const load: PageLoad = async (event) => {
     return loadRecipePage(event.params.slug);
