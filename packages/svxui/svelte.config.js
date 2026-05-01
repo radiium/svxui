4780d6008e0a2e913cd5e5edd650a3b9fcebf022
radiium <remip.dev@gmail.com>
2026-05-01 20:53:11 +0200

    chore: update packages & migrate to typescript 6

--- packages/svxui/svelte.config.js
+++ packages/svxui/svelte.config.js
@@ -8,6 +8,10 @@ process.env.PUBLIC_PKG_HOMEPAGE = process.env.npm_package_homepage;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+    },
     kit: {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
